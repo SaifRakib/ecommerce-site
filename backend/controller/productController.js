63a076b05db7a37dbf0fc5c059a89a23b073de1c
Saif Rakib Hasan <saifrakib99@gmail.com>
2022-01-17 13:00:41 +0600

    create find,filter,search and pagination

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // create product --admin
 
@@ -15,7 +16,14 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all Product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+
+  const apifeatures = ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apifeatures.query;
   res.status(200).json({ message: "Route is working" });
 });
 
@@ -30,6 +38,7 @@ exports.getProductsDetails = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+    productCount,
   });
 });
 
